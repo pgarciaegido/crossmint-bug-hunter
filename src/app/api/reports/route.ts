e0ee08cadb94e4b6aa2deaebfd89240d8ce737e8
Pablo Egido <peg@paella.dev>
2024-04-17 17:23:24 -0500

    test

--- src/app/api/reports/route.ts
+++ src/app/api/reports/route.ts
@@ -1,9 +1,11 @@
 import { FirebaseAdapter } from "@/services/FirebaseAdapter";
+import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 export const fetchCache = "force-no-store";
 
 export async function GET() {
+  revalidatePath("/api/reports");
   const reports = await new FirebaseAdapter().getFromDB("bug_report");
 
   return NextResponse.json({ status: "OK", data: reports }, { status: 200 });
